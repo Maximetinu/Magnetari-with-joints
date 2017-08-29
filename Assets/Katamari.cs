d526d3b93479c5dfb8247b9c47f31726ce5fadcc
Maximetinu <maximetinu@gmail.com>
2017-08-29 06:43:41 +0200

    First and last commit

---
+++ Assets/Katamari.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Katamari : MonoBehaviour {
+
+	bool hasJoint;
+	void OnCollisionEnter(Collision collision) {
+		// CHECK HERE IF COLLISION IS WITH THE PLAYER OR ATTACHED CUBES, WE DON'T WANT FREE CUBES TO CONNECT EACH OTHER
+		if (collision.gameObject.GetComponent<Rigidbody>() != null && !hasJoint) {
+			gameObject.AddComponent<FixedJoint>();
+			gameObject.GetComponent<FixedJoint>().connectedBody = collision.rigidbody;
+			hasJoint = true;
+		}
+	}
+}
+
+
+// Bonus level: since Magnetari is about magnetism, why don't add fake magnetism forces (like gravity) to the balls? 
+// Could be a skill with long cooldown, yes...
